e19c5572bf2843aba6b8e898a8de0967e9810d87
Mikhail Sergeev <msergeev06@gmail.com>
2018-03-10 22:44:20 +0300

    Основной функционал модуля готов

---
+++ install/install.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Файл установки модуля
+ *
+ * @package Ms\Dates
+ * @author Mikhail Sergeev <msergeev06@gmail.com>
+ * @copyright 2018 Mikhail Sergeev
+ */
+
+use Ms\Core\Lib\Installer;
+
+return (Installer::createModuleTables('ms.dates'));
---
+++ loc/ru/tables/work_calendar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Файл русской локализации модуля ms.dates
+ *
+ * @package Ms\Dates
+ * @author Mikhail Sergeev <msergeev06@gmail.com>
+ * @copyright 2018 Mikhail Sergeev
+ */
+
+return array (
+	'table_title' => 'Производственный календарь',
+	'field_date' => 'Дата',
+	'field_weekend' => 'Признак выходного дня'
+);
