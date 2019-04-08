f815e6c7a7c8c1c0b638296267dd680871783f4c
Mikhail Sergeev <msergeev06@gmail.com>
2019-04-08 23:52:06 +0300

    Новые числа на 2019 год

---
+++ updates/v_1_1_0/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Файл описания обновления версии 1.1.0 модуля ms.dates
+ *
+ * @package Ms\Dates
+ * @author Mikhail Sergeev <msergeev06@gmail.com>
+ * @copyright 2019 Mikhail Sergeev
+ */
+
+return array (
+	'REQUIRED_VERSIONS' => array (),
+	'UPDATE_INFO' => array (
+		'Добавлен производственный календарь за 2019 год'
+	)
+);
